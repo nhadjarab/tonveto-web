e5a4ad6b409cd3b09ac0b4af7e3622d49e5f5fe1
Nassim HADJ-ARAB <nassim.hadjarab@gmail.com>
2022-09-01 13:32:10 +0300

    Initial project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div className='bg-bgColor'>
+      <span>VetoLib</span>
+    </div>
+  )
+}
+
+export default Home
