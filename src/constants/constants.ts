8bb15fe10f0008fc35392f6b4f15fadc59d25f47
Nassim HADJ-ARAB <nassim.hadjarab@gmail.com>
2022-09-30 01:46:42 +0300

    Update api url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:3005";
-export const BACKEND_URL = "https://vetolib-backend-production.up.railway.app";
+export const BACKEND_URL = "https://api.tonveto.com";
